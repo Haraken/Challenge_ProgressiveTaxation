caeb8bea6af0d73f6ce009dded1a1cfb8e3d216c
Stephane Letarte Tremblay <ltart.stephane@gmail.com>
2020-07-04 16:42:47 -0400

    Completed Challenge with some comments.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_ProgressiveTaxation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Brackets taxBrackets = new Brackets();
+            string userInput;
+            float income;
+
+            Console.WriteLine("Taxation calculator per brackets.");
+            Console.WriteLine("Default brackets used in the code");
+            Console.WriteLine(" 10,000       ||    0.00 (0%)");
+            Console.WriteLine(" 30,000       ||    0.10 (10%)");
+            Console.WriteLine("100,000       ||    0.25 (25%)");
+            Console.WriteLine("Over 100,000  ||    0.40 (40%)");
+            Console.Write("Enter income to process and calculate tax due: ");
+
+            userInput = Console.ReadLine();
+
+            if (float.TryParse(userInput, out income))
+            {
+                Console.WriteLine();
+                Console.WriteLine("Processing {0}", userInput);
+                Console.WriteLine();
+                Console.WriteLine("The amount of tax due based on income is -> {0}", taxBrackets.TaxCalculation(income));
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Invalid user input! restart application.");
+            }
+
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ brackets.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Challenge_ProgressiveTaxation
+{
+    internal class Brackets
+    {
+        public float[][] TaxBrackets   // property
+        { get; set; }
+
+        //Basic contructor with pre-determined default informations
+        public Brackets()
+        {
+            Initialize(new float[][] {
+                new float[] { 10000f, 30000f, 100000f },
+                new float[] { 0f, 0.10f, 0.25f, 0.40f }
+            });
+        }
+
+        //Made different constructors while thinking how to write the TaxCalculation as seamless as possible.
+        public Brackets(float[] bracketStarts, float[] taxRate)
+        {
+            Initialize(new float[][] { bracketStarts, taxRate });
+        }
+
+        public Brackets(float[][] bracketRanges)
+        {
+            Initialize(bracketRanges);
+        }
+
+        private void Initialize(float[][] bracketRanges)
+        {
+            TaxBrackets = bracketRanges;
+        }
+
+        public float TaxCalculation(float income)
+        {
+            //TaxIndex will determinate which tax rate to apply
+            int taxIndex = 0;
+            for (int i = 0; i < TaxBrackets.GetLength(0); i++)
+            {
+                //If the income is supperior to the limit of the tax bracket the index is moved to the next one.
+                if (income > TaxBrackets[0][i])
+                    taxIndex = i + 1;
+            }
+
+            //Retuning the value of tax due based on the income and it's place in the taxation brackets.
+            return income * TaxBrackets[1][taxIndex];
+        }
+    }
+}
